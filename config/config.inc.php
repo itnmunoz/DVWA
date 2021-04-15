ef97c5de1459834135ac2af5fbd52e39ec75e54a
Nicolas Muñoz <itnmunoz@gmail.com>
2021-04-15 18:43:26 +0200

    Fix error db password

--- config/config.inc.php
+++ config/config.inc.php
@@ -18,7 +18,7 @@ $_DVWA = array();
 $_DVWA[ 'db_server' ]   = 'mysqldvwa';
 $_DVWA[ 'db_database' ] = 'dvwa';
 $_DVWA[ 'db_user' ]     = 'dvwa';
-$_DVWA[ 'db_password' ] = 'p@ssw0rd';
+$_DVWA[ 'db_password' ] = 'p@$$w0rd';
 $_DVWA[ 'db_port'] = '3306';
 
 # ReCAPTCHA settings
